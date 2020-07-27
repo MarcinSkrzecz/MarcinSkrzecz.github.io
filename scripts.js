c6c62a43252e9aa3d202e461ce0be391f6509636
MarcinSkrzecz <marpiowawa@o2.pl>
2020-07-27 18:10:57 +0200

    v2
    v2

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://glacial-ocean-44240.herokuapp.com/v1/task/';
+  var apiRoot = 'https://glacial-ocean-44240.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
