b8e1d7d90ff5191ba35f8fd44246eb3385b1cc1a
MarcinSkrzecz <58010162+MarcinSkrzecz@users.noreply.github.com>
2020-09-15 22:05:42 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://glacial-ocean-44240.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://glacial-ocean-44240.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://glacial-ocean-44240.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
